1ac997495b98ba0df395dd3abeb98bc86190b5d8
Radu <radupopa028@gmail.com>
2025-03-09 21:54:50 +0200

    feat: add method to add materials to the inventory

--- Inventory.h
+++ Inventory.h
@@ -6,6 +6,13 @@
 // This will be the "manager" class that handles the materials and operations
 class Inventory
 {
+   public:
+    // Add a new material to the inventory
+    void addMaterial(const Material &material)
+    {
+        m_materials.push_back(material);
+    }
+
    private:
     std::vector<Material> m_materials;  // List of materials in the inventory
     std::vector<Operation>
