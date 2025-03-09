e8106c9bc1a40d29e3bea4c797b048a28720eac0
Radu <radupopa028@gmail.com>
2025-03-09 21:18:05 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Product
+{
+   public:
+   private:
+    int cantitateValabila, pret;
+};
+
+class Material
+{
+};
+
+class Warehouse
+{
+};
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
